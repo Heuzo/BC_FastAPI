fab9f1f030e5a04673bc31929cd20b3f3c8ce23d
Heuzo <vlad.neight21@gmail.com>
2023-09-16 02:25:28 +0300

    new and compact req.txt and .gitignore

--- settings.py
+++ settings.py
@@ -1,8 +1,6 @@
 import os
 from hashlib import md5
 from random import randbytes
-from os import getenv
-
 
 SECRET_KEY = os.getenv('JWT_KEY', f'{md5(randbytes(128))}')
 JWT_ALGORITHM = os.getenv('JWT_ALGORITHM', 'HS256')
