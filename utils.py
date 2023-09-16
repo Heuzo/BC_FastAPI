67dddb16015431df1215c2b7a40d191c65bda1b7
Heuzo <vlad.neight21@gmail.com>
2023-09-16 03:44:41 +0300

    issues with recieving oauth bearer token

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from hashlib import md5
 
 import jwt
@@ -5,7 +6,7 @@ from fastapi import HTTPException, status
 
 from fake_db import fake_users
 from models import User
-from settings import JWT_ALGORITHM, SECRET_KEY
+from settings import ACCESS_TOKEN_EXPIRE_MINUTES, JWT_ALGORITHM, SECRET_KEY
 
 
 # Аутентификация на основании сверки хешей Логина / Пароля
@@ -23,8 +24,13 @@ def check_auth(user: User) -> bool:
 
 
 # Функция для создания JWT токена
-def create_jwt_token(payload) -> str:
-    encoded_jwt = jwt.encode(payload, SECRET_KEY, JWT_ALGORITHM)  # Кодируем токен
+def create_jwt_token(payload: dict) -> str:
+    body = payload.copy()
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES  # UTC.NOW + LifeTimeToken = Expiration
+    )
+    body.update({'exp': expire})  # Добавляем в токен парамтр срока жизни токена
+    encoded_jwt = jwt.encode(body, SECRET_KEY, JWT_ALGORITHM)  # Кодируем токен
     return encoded_jwt
 
 
@@ -43,3 +49,23 @@ def read_jwt_token(token: str, claim: str | None = None) -> str:
 
     except jwt.InvalidTokenError:  # Обработка неверного токена
         HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Token is invalid')
+
+
+def register_user(user: User):
+    # Получаем хеши ID/Пароля из JSON для дальнейшей обработки
+    hash_id = md5(user.user_name.encode()).hexdigest()
+    hash_pass = md5(user.password.encode()).hexdigest()
+
+    # Если пользователя еще нет в системе - заносим в базу
+    if hash_id not in fake_users:
+        fake_users[hash_id] = {
+            'user_name': f'{user.user_name}',
+            'password': f'{hash_pass}',
+        }
+
+    # Иначе - выпуливаем эксепшен
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User with that login already exists',
+        )
