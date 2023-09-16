9bc439ca17cd4f5c0398f9014e3c0f7c57aded88
Heuzo <vlad.neight21@gmail.com>
2023-09-16 17:23:54 +0300

    jwt auth step is done

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 from hashlib import md5
 from random import randbytes
 
-SECRET_KEY = os.getenv('JWT_KEY', f'{md5(randbytes(128))}')
+SECRET_KEY = os.getenv('JWT_KEY', f'{md5(randbytes(128)).hexdigest()}')
 JWT_ALGORITHM = os.getenv('JWT_ALGORITHM', 'HS256')
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
