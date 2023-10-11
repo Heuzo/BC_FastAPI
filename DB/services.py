c4388f755296a2019b5d4d6e563666ebd2aa5d59
Heuzo <vlad.neight21@gmail.com>
2023-10-11 16:19:38 +0300

    dir with psql image added to index

--- DB/services.py
+++ DB/services.py
@@ -1,11 +1,15 @@
-from . import database, models, schemas
 from typing import List
-from sqlalchemy import orm, delete
+
 from fastapi import HTTPException
+from sqlalchemy import orm
+
+from . import database, models, schemas
+
 
 # Создать таблицы из всех моделей
 def _add_tables():
     return database.Base.metadata.create_all(bind=database.engine)
+
 
 # Удалить все сущетсвующие таблицы
 def _drop_tables():
@@ -20,6 +24,7 @@ def get_db():
     finally:
         db.close()
 
+
 # Создание записи
 async def create_todo(todo: schemas.CreateTodo, db: orm.Session) -> schemas.Todo:
     todo = models.Todo(**todo.model_dump())
@@ -27,6 +32,7 @@ async def create_todo(todo: schemas.CreateTodo, db: orm.Session) -> schemas.Todo
     db.commit()
     db.refresh(todo)
     return schemas.Todo.from_orm(todo)
+
 
 # Поиск всех имеющитхся записей
 async def get_all_todos(db: orm.Session, reusable: bool = False) -> List[schemas.Todo]:
@@ -36,29 +42,34 @@ async def get_all_todos(db: orm.Session, reusable: bool = False) -> List[schemas
     else:
         return list(map(schemas.Todo.from_orm, todos))
 
+
 # Поиск одной записи по ID
 async def get_one_todo(db: orm.Session, todo_id: int) -> schemas.Todo:
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     return todo
 
+
 # Поиск удаление записи
 async def delete_one_todo(todo, db: orm.Session):
     db.delete(todo)
     db.commit()
-    
+
+
 # Удаление всех записей таблицы ToDo
 async def delete_all_todo(db: orm.Session):
     todos = await get_all_todos(db=db, reusable=True)
     if todos is None:
-        raise HTTPException(status_code=404, detail="All ToDo`s already deleted")
-        
+        raise HTTPException(status_code=404, detail='All ToDo`s already deleted')
+
     for item in todos:
         await delete_one_todo(item, db=db)
 
-    return {"message":"All ToDo raws deleted"}
+    return {'message': 'All ToDo raws deleted'}
 
 
-async def update_todo(db: orm.Session, todo_data: schemas.CreateTodo, todo: models.Todo):
+async def update_todo(
+    db: orm.Session, todo_data: schemas.CreateTodo, todo: models.Todo
+):
     todo.title = todo_data.title
     todo.description = todo_data.description
     todo.finished = todo_data.finished
--- main.py
+++ main.py
@@ -2,28 +2,33 @@ from datetime import datetime
 from typing import TYPE_CHECKING, Annotated, List
 
 import sqlalchemy.orm as _orm
-from fastapi import Depends, FastAPI, Response, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, Response
 from fastapi.responses import FileResponse
 from fastapi.security import HTTPBasic, OAuth2PasswordRequestForm
 
-import DB.models as models
 import DB.schemas as schemas
 import DB.services as services
-from utils import auth_and_token, is_admin, oauth2_scheme, read_jwt_token
+from utils import auth_and_token, is_admin, read_jwt_token
 
-app = FastAPI(title='Heuzon API', description='Документация к сервису, реализованному в рамках обучения')
+app = FastAPI(
+    title='Heuzon API',
+    description='Документация к сервису, реализованному в рамках обучения',
+)
 security = HTTPBasic()
 
 if TYPE_CHECKING:
     pass
 
+
 @app.on_event('startup')
 def prepare_base():
     services._add_tables()
 
+
 @app.on_event('shutdown')
 def clean_up_base():
     services._drop_tables()
+
 
 @app.get('/')
 async def main_page(response: Response):
@@ -33,7 +38,8 @@ async def main_page(response: Response):
     response.set_cookie(key='last_visit', value=str(now))
     return FileResponse('Front/index.html')
 
-@app.post('/api/login',  tags=['Auth'])
+
+@app.post('/api/login', tags=['Auth'])
 async def login(
     user: Annotated[OAuth2PasswordRequestForm, Depends()], response: Response
 ):
@@ -51,53 +57,61 @@ async def protected(token=Depends(read_jwt_token)):
     return {'message': 'Success'}
 
 
-# ----------------- CRUD ------------------- 
+# ----------------- CRUD -------------------
+
 
 @app.post('/api/todo', response_model=schemas.Todo, tags=['ToDo CRUD'])
 async def create_todo(
     todo: schemas.CreateTodo, db: _orm.Session = Depends(services.get_db)
-    ):
+):
     return await services.create_todo(todo=todo, db=db)
+
 
 @app.get('/api/todo', response_model=List[schemas.Todo], tags=['ToDo CRUD'])
 async def get_all_todo(db: _orm.Session = Depends(services.get_db)):
     return await services.get_all_todos(db=db)
 
+
 @app.get('/api/todo/{todo_id}', response_model=schemas.Todo, tags=['ToDo CRUD'])
 async def get_todo(todo_id: int, db: _orm.Session = Depends(services.get_db)):
     return await services.get_one_todo(db=db, todo_id=todo_id)
 
-#TODO: Вынести логику обновления пользователя в сервисы
+
+# TODO: Вынести логику обновления пользователя в сервисы
 @app.put('/api/todo/{todo_id}', response_model=schemas.Todo)
-async def update_todo(todo_id: int, data: schemas.CreateTodo, db : _orm.Session = Depends(services.get_db)):
+async def update_todo(
+    todo_id: int, data: schemas.CreateTodo, db: _orm.Session = Depends(services.get_db)
+):
     todo = await services.get_one_todo(db=db, todo_id=todo_id)
     if todo is None:
-        raise HTTPException(status_code=404, detail="ToDo does not exist")
-    
+        raise HTTPException(status_code=404, detail='ToDo does not exist')
+
     return await services.update_todo(db=db, todo=todo, todo_data=data)
 
-#TODO Вынести логику удаления пользователя в сервисы
+
+# TODO Вынести логику удаления пользователя в сервисы
 @app.delete('/api/todo/{todo_id}', tags=['ToDo CRUD'])
 async def delete_todo(todo_id: int, db: _orm.Session = Depends(services.get_db)):
     todo = await services.get_one_todo(db=db, todo_id=todo_id)
     if todo is None:
-        raise HTTPException(status_code=404, detail="ToDo does not exist")
-    
+        raise HTTPException(status_code=404, detail='ToDo does not exist')
+
     await services.delete_one_todo(todo, db=db)
 
 
 @app.delete('/api/todo', tags=['ToDo CRUD'])
-async def delete_todo(db: _orm.Session = Depends(services.get_db)):
+async def delete_all_todo(db: _orm.Session = Depends(services.get_db)):
     return await services.delete_all_todo(db=db)
 
 
-# ----------------- END OF CRUD ------------------- 
+# ----------------- END OF CRUD -------------------
 
 
 # Роуты овтечащие за раздачу файлов статики по запросу фронта
 @app.get('/assets/{file_path:path}', tags=['Static'])
 async def css_static(file_path: str):
     return FileResponse(f'Front/assets/{file_path}')
+
 
 @app.get('/images/{file_path:path}', tags=['Static'])
 async def images_static(file_path: str):
